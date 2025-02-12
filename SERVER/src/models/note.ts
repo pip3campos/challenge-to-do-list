f322e4f348b9d780e5e3fc81f325950d1caad7ec
pip3campos <felipeacampose@gmail.com>
2025-02-11 21:39:01 -0300

    tasks routes protected

--- SERVER/src/models/note.ts
+++ SERVER/src/models/note.ts
@@ -1,6 +1,7 @@
-import { InferSchemaType, model, Schema } from "mongoose";
+import { InferSchemaType, model, Schema, Types } from "mongoose";
 
 const noteSchema = new Schema({
+    author_id: { type: Types.ObjectId, required: true },
     title: { type: String, required: true },
     text: { type: String },
 }, { timestamps: true });
--- SERVER/src/routes/tasks.ts
+++ SERVER/src/routes/tasks.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import * as NotesController from '../controllers/notes'
+import passport from '../middlewares/passport'
 
 const router = Router();
 
-router.get('/', NotesController.getNotes);
-router.get('/:noteId', NotesController.getNote);
-router.post('/', NotesController.createNote);
-router.patch('/:noteId', NotesController.updateNote);
-router.delete('/:noteId', NotesController.deleteNote);
+router.get('/', passport.authenticate('jwt', { session: false }), NotesController.getNotes);
+router.get('/:noteId', passport.authenticate('jwt', { session: false }), NotesController.getNote);
+router.post('/', passport.authenticate('jwt', { session: false }), NotesController.createNote);
+router.patch('/:noteId', passport.authenticate('jwt', { session: false }), NotesController.updateNote);
+router.delete('/:noteId', passport.authenticate('jwt', { session: false }), NotesController.deleteNote);
 
 export default router;
